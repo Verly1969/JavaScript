1f30e6f81970c7add0c7939e395c97d4f85c2572
Verly1969 <eddyverly@icloud.com>
2026-02-04 13:05:49 +0100

    modification du select couleurs par teinte

--- exo_9/script.js
+++ exo_9/script.js
@@ -7,9 +7,26 @@ window.addEventListener("DOMContentLoaded", () => {
     const colorAlea = document.getElementById("colorAlea");
     const colorHue = document.getElementById("colorHue");
     const boxColors = document.getElementById("boxColors");
+    const select = document.getElementById("select");
+    const pMessage = document.getElementById("pMessage");
+
+    const colorTab = 
+        [
+            "red", 
+            "pink", 
+            "purple", 
+            "navy", 
+            "blue", 
+            "aqua", 
+            "green", 
+            "lime", 
+            "yellow", 
+            "orange"
+        ];
 
     const url = `https://x-colors.yurace.pro/api/`;
 
+    //#region Fonction
     async function randomColor(){
         const randomUrl = url + `random`;
         console.log(`Random url: ${randomUrl}`);
@@ -22,10 +39,14 @@ window.addEventListener("DOMContentLoaded", () => {
         colorAlea.style.backgroundColor = color.hex;
     }
 
+    /**
+     * Fonction qui affiche 10 nuances de couleur
+     * @param {string} nuance 
+     */
     async function nuanceAlea(nuance){
         const nuanceUrl = `${url}random/${nuance}`;
         console.log(nuanceUrl);
-
+        pMessage.innerText = " ⇊ click droit sur la nuance pour afficher les infos ⇊";
         for (let i = 0; i < 10; i++)
         {
             const response = await fetch(nuanceUrl);
@@ -33,13 +54,62 @@ window.addEventListener("DOMContentLoaded", () => {
             const div = document.createElement("div");
             div.classList.add("colortest");
             div.style.backgroundColor = color.hex;
+            div.style.border = "solid 1px black";
             boxColors.append(div);
             console.log(color);
         }
     }
 
+    function displayDetails(hex, rgb, hsl){
+        
+    }
+
+    /**
+     * Fonction de remplissage du select --> couleurs par teinte
+     */
+    function loadSelect(){
+        for (let i = 0; i < colorTab.length; i++) {
+            const option = document.createElement("option");
+            option.style.backgroundColor = colorTab[i];
+            option.setAttribute("value", colorTab[i]);
+            select.append(option);
+        }
+    }
+
+    function selectHue(){
+        
+    }
+
+    /**
+     * Fonction qui supprime les divs enfants des nuances
+     */
+    function deleteDiv(){
+        if (boxColors.childElementCount !== 0){
+            for (let i = 0; i < 10; i++){
+                boxColors.lastElementChild.remove();
+            }
+        }
+    }
+
+    //#endregion
+
+    //#region Event
     btnAlea.addEventListener("click", randomColor);
 
-    nuanceAlea("000");
-    
+    select.addEventListener("change", () => {
+        deleteDiv();
+        const element = select.selectedIndex;
+        if (element !== 0){
+            colorHue.style.backgroundColor = select[element].value;
+            console.log(select.value);
+            nuanceAlea(select.value);
+            console.log(`Elément sélectionné: ${element}`);
+        }
+        select.selectedIndex = 0;
+    })
+
+    //#endregion
+
+    loadSelect();
+    console.log(`Nombre d'enfants: ${boxColors.childElementCount}`)
 })
